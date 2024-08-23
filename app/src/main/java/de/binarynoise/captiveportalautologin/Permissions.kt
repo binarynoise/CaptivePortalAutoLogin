cef6337a1b61ded8da6f87095aeaf1d82c33201d
binarynoise <binarynoise@mail.de>
2024-08-23 18:42:59 +0200

    fun with permissions

--- app/src/main/java/de/binarynoise/captiveportalautologin/Permissions.kt
+++ app/src/main/java/de/binarynoise/captiveportalautologin/Permissions.kt
@@ -1,6 +1,7 @@
 package de.binarynoise.captiveportalautologin
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -8,8 +9,8 @@ import android.net.Uri
 import android.os.Build
 import android.os.UserManager
 import android.provider.Settings
-import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
 import androidx.annotation.StringRes
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -23,35 +24,55 @@ class Permission private constructor(
     val request: (ComponentActivity) -> Unit,
     val enabled: (Context) -> Boolean,
 ) {
+    private constructor(
+        name: String?,
+        @StringRes nameRes: Int?,
+        description: String?,
+        @StringRes descriptionRes: Int?,
+        granted: (Context) -> Boolean,
+        request: (ComponentActivity) -> Unit,
+        enabled: (Context) -> Boolean = { true },
+        minSdk: Int = 0,
+    ) : this(
+        name,
+        nameRes,
+        description,
+        descriptionRes,
+        if (minSdk == 0) granted else { context -> (Build.VERSION.SDK_INT < minSdk) || granted(context) },
+        if (minSdk == 0) request else { componentActivity -> if (Build.VERSION.SDK_INT >= minSdk) request(componentActivity) },
+        enabled,
+    )
+    
     constructor(
         name: String,
         description: String,
         granted: (Context) -> Boolean,
         request: (ComponentActivity) -> Unit,
-        enabled: (Context) -> Boolean = { true }
-    ) : this(name, null, description, null, granted, request, enabled)
+        enabled: (Context) -> Boolean = { true },
+        minSdk: Int = 0,
+    ) : this(name, null, description, null, granted, request, enabled, minSdk)
     
     constructor(
         nameRes: Int,
         descriptionRes: Int,
         granted: (Context) -> Boolean,
         request: (ComponentActivity) -> Unit,
-        enabled: (Context) -> Boolean = { true }
-    ) : this(null, nameRes, null, descriptionRes, granted, request, enabled)
+        enabled: (Context) -> Boolean = { true },
+        minSdk: Int = 0,
+    ) : this(null, nameRes, null, descriptionRes, granted, request, enabled, minSdk)
 }
 
 object Permissions {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     val notifications = Permission(
         "Send Notifications", "Show a persistent status notification and show little messages at the bottom of the screen",
         { context ->
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return@Permission true
             ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
         },
         { componentActivity ->
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return@Permission
-            
             ActivityCompat.requestPermissions(componentActivity, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0)
         },
+        minSdk = Build.VERSION_CODES.TIRAMISU,
     )
     
     val fineLocation = Permission(
@@ -65,20 +86,20 @@ object Permissions {
         },
     )
     
-    val backgroundLocation = Permission("Background Location", "Collect the SSID of Portals. Required for the background service", { context ->
-        if (Build.VERSION.SDK_INT < 34) return@Permission true
-        ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }, { componentActivity ->
-        if (Build.VERSION.SDK_INT < 29) return@Permission
-        if (!fineLocation.granted(componentActivity)) {
-            Toast.makeText(componentActivity, "Cannot request background location without fine location permission", Toast.LENGTH_LONG).show()
-            return@Permission
-        }
-        
-        ActivityCompat.requestPermissions(componentActivity, arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION), 0)
-    }, { context ->
-        fineLocation.granted(context)
-    })
+    @RequiresApi(Build.VERSION_CODES.Q)
+    val backgroundLocation = Permission(
+        "Background Location", "Collect the SSID of Portals. Required for the background service",
+        { context ->
+            ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+        },
+        { componentActivity ->
+            ActivityCompat.requestPermissions(componentActivity, arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION), 0)
+        },
+        { context ->
+            fineLocation.granted(context)
+        },
+        minSdk = Build.VERSION_CODES.Q,
+    )
     
     val openSettings = Permission(
         "Open Settings",
@@ -95,6 +116,7 @@ object Permissions {
         },
     )
     
+    @SuppressLint("NewApi")
     val all: List<Permission> = listOf(
         notifications,
         fineLocation,
