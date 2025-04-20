113da1c1b88d9120a0b4265fdfa9f14995944fc0
binarynoise <binarynoise@mail.de>
2025-04-20 21:31:53 +0200

    Unify loading of Xposed modules in XposedInit

--- app/src/main/kotlin/de/binarynoise/captiveportalautologin/xposed/LocationIndicatorHook.kt
+++ app/src/main/kotlin/de/binarynoise/captiveportalautologin/xposed/LocationIndicatorHook.kt
@@ -1,20 +1,20 @@
 package de.binarynoise.captiveportalautologin.xposed
 
 import de.binarynoise.captiveportalautologin.BuildConfig
+import de.binarynoise.liberator.cast
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import de.robv.android.xposed.XC_MethodHook as MethodHook
+
 
 class LocationIndicatorHook : IXposedHookLoadPackage {
-    private val PACKAGE_SYSTEMUI = "com.android.systemui"
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        if (lpparam.packageName != PACKAGE_SYSTEMUI) return
         XposedHelpers.findAndHookMethod(
-            "$PACKAGE_SYSTEMUI.appops.AppOpsControllerImpl", lpparam.classLoader, "getActiveAppOps", Boolean::class.java,
-            object : XC_MethodHook() {
+            "com.android.systemui.appops.AppOpsControllerImpl", lpparam.classLoader, "getActiveAppOps", Boolean::class.java,
+            object : MethodHook() {
                 override fun afterHookedMethod(param: MethodHookParam) {
-                    param.result = (param.result as List<*>).filter { appOpItem ->
+                    param.result = param.result.cast<MutableList<*>>().filter { appOpItem ->
                         !XposedHelpers.getObjectField(appOpItem, "mPackageName").equals(BuildConfig.APPLICATION_ID)
                     }
                 }
