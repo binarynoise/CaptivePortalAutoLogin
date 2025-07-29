d8da9d67e67b581a5576fcc631055564b63f9673
binarynoise <binarynoise@mail.de>
2025-07-29 16:33:39 +0200

    Add StrictMode policy with detailed logging for API 28+

--- app/src/debug/kotlin/de/binarynoise/captiveportalautologin/DebugApplication.kt
+++ app/src/debug/kotlin/de/binarynoise/captiveportalautologin/DebugApplication.kt
@@ -1,11 +1,27 @@
 package de.binarynoise.captiveportalautologin
 
+import java.util.concurrent.*
+import android.os.Build
 import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
+import android.widget.Toast
+import de.binarynoise.logger.Logger.log
 
 class DebugApplication : Application() {
+    private val penaltyExecutor: Executor = Executors.newSingleThreadScheduledExecutor()
+    
     override fun onCreate() {
         super.onCreate()
-        StrictMode.setVmPolicy(VmPolicy.Builder(StrictMode.getVmPolicy()).detectLeakedClosableObjects().build())
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder().apply {
+                detectAll()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    penaltyListener(penaltyExecutor) { v ->
+                        log("StrictMode policy violation", v)
+                        Toast.makeText(applicationContext, v.message ?: "StrictMode policy violation", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }.build()
+        )
     }
 }
