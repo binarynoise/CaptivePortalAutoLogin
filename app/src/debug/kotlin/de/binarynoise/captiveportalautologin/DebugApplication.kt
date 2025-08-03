3991102e2f37675ac04a7a2eb46806bb5c50d837
binarynoise <binarynoise@mail.de>
2025-08-03 15:44:52 +0200

    Improve strict mode violation logging

--- app/src/debug/kotlin/de/binarynoise/captiveportalautologin/DebugApplication.kt
+++ app/src/debug/kotlin/de/binarynoise/captiveportalautologin/DebugApplication.kt
@@ -4,6 +4,8 @@ import java.util.concurrent.*
 import android.os.Build
 import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
+import android.os.strictmode.DiskReadViolation
+import android.os.strictmode.UntaggedSocketViolation
 import android.widget.Toast
 import de.binarynoise.logger.Logger.log
 
@@ -17,11 +19,38 @@ class DebugApplication : Application() {
                 detectAll()
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                     penaltyListener(penaltyExecutor) { v ->
-                        log("StrictMode policy violation", v)
-                        Toast.makeText(applicationContext, v.message ?: "StrictMode policy violation", Toast.LENGTH_SHORT).show()
+                        if (v is UntaggedSocketViolation) return@penaltyListener
+                        
+                        log("StrictMode VmPolicy violation", v)
+                        val errorMessage = v.message ?: "StrictMode policy violation"
+                        Toast.makeText(applicationContext, errorMessage, Toast.LENGTH_SHORT).show()
                     }
                 }
             }.build()
         )
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().apply {
+            detectAll()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                penaltyListener(penaltyExecutor) { v ->
+                    if (v is DiskReadViolation) {
+                        val allowed = v.stackTrace.any { stackTraceElement ->
+                            arrayOf(
+                                "androidx.preference.Preference",
+                                "androidx.preference.PreferenceManager",
+                            ).any { className ->
+                                className == stackTraceElement.className
+                            }
+                        }
+                        if (allowed) {
+                            return@penaltyListener
+                        }
+                    }
+                    
+                    log("StrictMode ThreadPolicy violation", v)
+                    val errorMessage = v.message ?: "StrictMode policy violation"
+                    Toast.makeText(applicationContext, errorMessage, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }.build())
     }
 }
