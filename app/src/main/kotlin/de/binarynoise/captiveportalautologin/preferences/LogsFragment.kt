6418203bf9cca30ec0f257c855c6e9993f7dd9bd
binarynoise <binarynoise@mail.de>
2025-08-03 15:45:44 +0200

    move file IO away from UI Thread

--- app/src/main/kotlin/de/binarynoise/captiveportalautologin/preferences/LogsFragment.kt
+++ app/src/main/kotlin/de/binarynoise/captiveportalautologin/preferences/LogsFragment.kt
@@ -1,7 +1,14 @@
 package de.binarynoise.captiveportalautologin.preferences
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.preference.PreferenceCategory
 import de.binarynoise.captiveportalautologin.R
 import de.binarynoise.captiveportalautologin.databinding.ItemLogExportBinding
@@ -17,42 +24,59 @@ class LogsFragment : AutoCleanupPreferenceFragment() {
             addPreference(PreferenceCategory(ctx)) {
                 title = "Export Logs"
                 
-                val logFiles = Logger.Config.folder?.listFiles()?.sortedByDescending { it.name }.orEmpty()
-                logFiles.forEach { file ->
-                    addPreference(WidgetPreference(ctx, R.layout.item_log_export) { view ->
-                        val binding = ItemLogExportBinding.bind(view)
-                        with(binding) {
-                            shareButton.setOnClickListener {
-                                try {
-                                    shareFile(file, "Share log")
-                                } catch (e: Exception) {
-                                    Toast.makeText(
-                                        view.context, "Failed to share file: ${e.message}", Toast.LENGTH_LONG
-                                    ).show()
-                                    log("Error sharing file (${file.name})", e)
+                lifecycleScope.launch {
+                    repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        val logFiles = withContext(Dispatchers.IO) {
+                            Logger.Config.folder?.listFiles()?.sortedByDescending { it.name }.orEmpty()
+                        }
+                        logFiles.forEach { file ->
+                            addPreference(WidgetPreference(ctx, R.layout.item_log_export) { view ->
+                                val binding = ItemLogExportBinding.bind(view)
+                                with(binding) {
+                                    shareButton.setOnClickListener {
+                                        lifecycleScope.launch {
+                                            try {
+                                                withContext(Dispatchers.IO) {
+                                                    shareFile(file, "Share log")
+                                                }
+                                            } catch (e: Exception) {
+                                                Toast.makeText(
+                                                    view.context,
+                                                    "Failed to share file: ${e.message}",
+                                                    Toast.LENGTH_LONG
+                                                ).show()
+                                                log("Error sharing file (${file.name})", e)
+                                            }
+                                        }
+                                    }
+                                    copyToSdButton.setOnClickListener {
+                                        lifecycleScope.launch {
+                                            try {
+                                                val toast =
+                                                    Toast.makeText(view.context, "Saving...", Toast.LENGTH_SHORT)
+                                                toast.show()
+                                                
+                                                withContext(Dispatchers.IO) {
+                                                    FileUtils.saveFileToSd(file, "text/plain", view.context)
+                                                }
+                                                
+                                                toast.cancel()
+                                                Toast.makeText(view.context, "Saved", Toast.LENGTH_SHORT).show()
+                                            } catch (e: Exception) {
+                                                Toast.makeText(
+                                                    view.context,
+                                                    e::class.java.simpleName + ": " + e.message + "\n" + "Please try again.",
+                                                    Toast.LENGTH_SHORT,
+                                                ).show()
+                                                log("Error saving file", e)
+                                            }
+                                        }
+                                    }
                                 }
-                            }
-                            copyToSdButton.setOnClickListener {
-                                try {
-                                    val toast = Toast.makeText(view.context, "Saving...", Toast.LENGTH_SHORT)
-                                    toast.show()
-                                    
-                                    FileUtils.saveFileToSd(file, "text/plain", view.context)
-                                    
-                                    toast.cancel()
-                                    Toast.makeText(view.context, "Saved", Toast.LENGTH_SHORT).show()
-                                } catch (e: Exception) {
-                                    Toast.makeText(
-                                        view.context,
-                                        e::class.java.simpleName + ": " + e.message + "\n" + "Please try again.",
-                                        Toast.LENGTH_SHORT,
-                                    ).show()
-                                    log("Error saving file", e)
-                                }
+                            }) {
+                                title = file.name
                             }
                         }
-                    }) {
-                        title = file.name
                     }
                 }
             }
