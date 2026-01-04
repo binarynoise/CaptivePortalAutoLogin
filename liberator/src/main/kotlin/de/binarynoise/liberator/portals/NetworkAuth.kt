b8e0c0abfe5f23359168b4ca55c76018ba8817f8
binarynoise <binarynoise@mail.de>
2026-01-04 23:54:30 +0100

    fix NetworkAuth

--- liberator/src/main/kotlin/de/binarynoise/liberator/portals/NetworkAuth.kt
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/NetworkAuth.kt
@@ -4,6 +4,11 @@ import de.binarynoise.liberator.PortalLiberator
 import de.binarynoise.util.okhttp.checkSuccess
 import de.binarynoise.util.okhttp.followRedirects
 import de.binarynoise.util.okhttp.get
+import de.binarynoise.util.okhttp.getInput
+import de.binarynoise.util.okhttp.getLocation
+import de.binarynoise.util.okhttp.parseHtml
+import de.binarynoise.util.okhttp.postForm
+import de.binarynoise.util.okhttp.requestUrl
 import okhttp3.Cookie
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
@@ -16,12 +21,26 @@ object NetworkAuth : PortalLiberator {
     }
     
     override fun solve(locationUrl: HttpUrl, client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
-        val continue_url = locationUrl.queryParameter("continue_url") ?: error("no continue_url")
+        val response1 = client.get(locationUrl, null)
+        val location1 = response1.getLocation() ?: error("no location1")
         
-        client.get(
-            locationUrl, "grant", queryParameters = mapOf(
-                "continue_url" to continue_url
-            )
-        ).followRedirects(client).checkSuccess()
+        val response2 = client.get(locationUrl, location1)
+        val html2 = response2.parseHtml()
+        val `csrf-token` = html2.getElementsByAttributeValue("name", "csrf-token").single().attr("content")
+        val `csrf-param` = html2.getElementsByAttributeValue("name", "csrf-param").single().attr("content")
+        
+        val form_action = html2.id("splash-form").attr("action")
+        val base_grant_url = html2.getInput("base_grant_url")
+        val success_url = html2.getInput("success_url")
+        
+        val response3 = client.postForm(
+            response2.requestUrl, form_action,
+            mapOf(
+                `csrf-param` to `csrf-token`,
+                "success_url" to success_url,
+                "base_grant_url" to base_grant_url,
+            ),
+        )
+        response3.followRedirects(client).checkSuccess()
     }
 }
