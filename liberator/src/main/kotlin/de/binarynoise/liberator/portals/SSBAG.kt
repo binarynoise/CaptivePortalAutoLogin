167a238a92d792145a71bf217180b67e320c4e40
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-01-16 14:18:46 +0100

    implement ssb ag guest wifi

---
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/SSBAG.kt
@@ -0,0 +1,32 @@
+package de.binarynoise.liberator.portals
+
+import de.binarynoise.liberator.PortalLiberator
+import de.binarynoise.liberator.SSID
+import de.binarynoise.util.okhttp.checkSuccess
+import de.binarynoise.util.okhttp.getInput
+import de.binarynoise.util.okhttp.parseHtml
+import de.binarynoise.util.okhttp.postForm
+import okhttp3.Cookie
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Response
+
+@Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
+@SSID("SSB fuer Dich - WiFi Free")
+object SSBAG : PortalLiberator {
+    override fun canSolve(locationUrl: HttpUrl, response: Response): Boolean {
+        return "gast11.ssb-ag.de" == locationUrl.host && !response.isRedirect
+    }
+    
+    override fun solve(locationUrl: HttpUrl, client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
+        val token = response.parseHtml().getInput("token")
+        client.postForm(
+            locationUrl,
+            "AupSubmit.action?from=AUP",
+            mapOf(
+                "token" to token,
+                "aupAccepted" to "true",
+            ),
+        ).checkSuccess()
+    }
+}
