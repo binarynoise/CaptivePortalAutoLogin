1aad25071e07891cc15d19a04390e532643b3423
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-01-20 22:01:43 +0100

    implement TheCloud captive portal

---
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/TheCloud.kt
@@ -0,0 +1,29 @@
+package de.binarynoise.liberator.portals
+
+import de.binarynoise.liberator.PortalLiberator
+import de.binarynoise.liberator.SSID
+import de.binarynoise.util.okhttp.checkSuccess
+import de.binarynoise.util.okhttp.followRedirects
+import de.binarynoise.util.okhttp.postForm
+import okhttp3.Cookie
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Response
+
+@Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
+@SSID(
+    "mycloud",
+    "o2 free Wifi",
+    "WiFi Darmstadt",
+)
+object TheCloud : PortalLiberator {
+    override fun canSolve(locationUrl: HttpUrl, response: Response): Boolean {
+        return locationUrl.host == "service.thecloud.eu"
+    }
+    
+    override fun solve(locationUrl: HttpUrl, client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
+        client.postForm(locationUrl, "macauthlogin/v1/registration", mapOf("terms" to "true"))
+            .followRedirects(client)
+            .checkSuccess()
+    }
+}
