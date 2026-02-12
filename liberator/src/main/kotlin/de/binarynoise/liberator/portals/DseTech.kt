58b9964f09d6d804cecf74ba51d8ad89555d99ff
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-02-12 18:55:52 +0100

    implement FortiAuthenticator
    also fixes DseTech

--- liberator/src/main/kotlin/de/binarynoise/liberator/portals/DseTech.kt
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/DseTech.kt
@@ -3,23 +3,21 @@ package de.binarynoise.liberator.portals
 import de.binarynoise.liberator.Experimental
 import de.binarynoise.liberator.PortalLiberator
 import de.binarynoise.liberator.SSID
-import de.binarynoise.rhino.RhinoParser
 import de.binarynoise.util.okhttp.checkSuccess
-import de.binarynoise.util.okhttp.firstPathSegment
 import de.binarynoise.util.okhttp.followRedirects
-import de.binarynoise.util.okhttp.get
-import de.binarynoise.util.okhttp.getInput
 import de.binarynoise.util.okhttp.parseHtml
 import de.binarynoise.util.okhttp.postForm
 import de.binarynoise.util.okhttp.requestUrl
+import de.binarynoise.util.okhttp.toParameterMap
 import okhttp3.Cookie
-import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Response
+import org.jsoup.nodes.FormElement
 
 @Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
 @Experimental
 @SSID("DeichmannGast")
+@FortiAuthenticatorSubPortal
 object DseTech : PortalLiberator {
     override fun canSolve(response: Response): Boolean {
         return response.requestUrl.host == "disclaimer.dse-tech.net"
@@ -27,41 +25,10 @@ object DseTech : PortalLiberator {
     
     override fun solve(client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
         val html = response.parseHtml()
-        val form = html.getElementsByTag("form")
+        val form = html.getElementsByTag("form").single() as FormElement
         if (!form.hasAttr("action")) error("no action url")
         val action = form.attr("action")
-        client.postForm(
-            null, action, mapOf(
-                "magic" to html.getInput("magic"),
-                "username" to html.getInput("username"),
-                "password" to html.getInput("password"),
-                "submit" to html.getInput("submit"),
-            )
-        ).followRedirects(client).checkSuccess()
-    }
-}
-
-object DseTechRedirect : PortalLiberator {
-    override fun canSolve(response: Response): Boolean {
-        if (DseTech.canSolve(response)) return false
-        if (response.isRedirect) return false
-        val html = response.parseHtml()
-        val script = html.getElementsByTag("script").first()?.data() ?: return false
-        val assignments = RhinoParser().parseAssignments(script)
-        val redirectUrl = assignments["window.location"]?.toHttpUrl() ?: return false
-        if (redirectUrl.firstPathSegment == "fgtauth") return true
-        if (redirectUrl.host == "disclaimer.dse-tech.net") return true
-        return false
-    }
-    
-    override fun solve(client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
-        val html = response.parseHtml()
-        val script = html.getElementsByTag("script").first()?.data() ?: error("no script")
-        val assignments = RhinoParser().parseAssignments(script)
-        val redirectUrl = assignments["window.location"]?.toHttpUrl() ?: error("no window.location")
-        val response = client.get(redirectUrl, null)
-        if (canSolve(response)) return solve(client, response, cookies)
-        if (DseTech.canSolve(response)) return DseTech.solve(client, response, cookies)
-        error("${this::class.java.name} couldn't find redirect successor")
+        val parameters = form.toParameterMap()
+        client.postForm(null, action, parameters).checkSuccess()
     }
 }
---
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/FortiAuthenticator.kt
@@ -0,0 +1,94 @@
+@file:Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
+
+package de.binarynoise.liberator.portals
+
+import de.binarynoise.liberator.Experimental
+import de.binarynoise.liberator.PortalLiberator
+import de.binarynoise.liberator.PortalLiberatorConfig
+import de.binarynoise.liberator.SSID
+import de.binarynoise.liberator.isExperimental
+import de.binarynoise.liberator.portals.FortiAuthenticator.isFortiAuthenticatorUrl
+import de.binarynoise.liberator.tryOrIgnore
+import de.binarynoise.logger.Logger.log
+import de.binarynoise.rhino.RhinoParser
+import de.binarynoise.util.okhttp.firstPathSegment
+import de.binarynoise.util.okhttp.get
+import de.binarynoise.util.okhttp.parseHtml
+import de.binarynoise.util.okhttp.postForm
+import de.binarynoise.util.okhttp.requestUrl
+import de.binarynoise.util.okhttp.toParameterMap
+import okhttp3.Cookie
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import org.jsoup.nodes.FormElement
+
+/**
+ * Identifies this [PortalLiberator] as a sub portal of [FortiAuthenticator],
+ * which makes it eligible for solving after a [FortiAuthenticatorRedirect].
+ */
+@Target(AnnotationTarget.CLASS)
+annotation class FortiAuthenticatorSubPortal
+
+@Experimental
+@SSID(
+    "ITS - FREEWIFI",
+    "Douglas Guest",
+)
+object FortiAuthenticator : PortalLiberator {
+    fun HttpUrl.isFortiAuthenticatorUrl(): Boolean {
+        return this.port == 1000 && this.firstPathSegment == "fgtauth"
+    }
+    
+    override fun canSolve(response: Response): Boolean {
+        if (!response.requestUrl.isFortiAuthenticatorUrl()) return false
+        if (response.isRedirect) return false
+        return false
+    }
+    
+    override fun solve(client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
+        val html = response.parseHtml()
+        val form = html.getElementsByTag("form").single() as FormElement
+        val parameters = form.toParameterMap()
+        client.postForm(
+            response.requestUrl, "/", parameters + mapOf(
+                "answer" to "1",
+            )
+        )
+    }
+}
+
+@Experimental
+object FortiAuthenticatorRedirect : PortalLiberator {
+    override fun canSolve(response: Response): Boolean {
+        if (response.isRedirect) return false
+        val html = response.parseHtml()
+        val script = html.getElementsByTag("script").first()?.data() ?: return false
+        val assignments = RhinoParser().parseAssignments(script)
+        val redirectUrl = assignments["window.location"]?.toHttpUrl() ?: return false
+        if (!redirectUrl.isFortiAuthenticatorUrl()) return false
+        return true
+    }
+    
+    override fun solve(client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
+        val html = response.parseHtml()
+        val script = html.getElementsByTag("script").first()?.data() ?: error("no script")
+        val assignments = RhinoParser().parseAssignments(script)
+        val redirectUrl = assignments["window.location"]?.toHttpUrl() ?: error("no window.location")
+        val response = client.get(redirectUrl, null)
+        if (canSolve(response)) return solve(client, response, cookies)
+        val viableSubPortals = allPortalLiberators //
+            .filter { solver -> PortalLiberatorConfig.experimental || !solver.isExperimental() }
+            .filter { solver -> solver::class.java.annotations.any { it is FortiAuthenticatorSubPortal } }
+            .filter { liberator -> liberator.canSolve(response) }
+        if (viableSubPortals.isEmpty()) error("${this::class.java.name} couldn't find redirect successor")
+        log("found ${viableSubPortals.size} viable ${FortiAuthenticatorSubPortal::class.java.name}s")
+        for (solver in viableSubPortals) {
+            log("executing solver ${solver::class.java.name}")
+            tryOrIgnore {
+                solver.solve(client, response, cookies)
+            }
+        }
+    }
+}
