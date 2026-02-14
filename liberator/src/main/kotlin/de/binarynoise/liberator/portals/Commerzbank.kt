717d7c6ded9f50f803818419da177d078d9c0bb5
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-02-14 22:27:04 +0100

    fix Commerzbank

--- liberator/src/main/kotlin/de/binarynoise/liberator/portals/Commerzbank.kt
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/Commerzbank.kt
@@ -1,7 +1,9 @@
 package de.binarynoise.liberator.portals
 
 import de.binarynoise.liberator.PortalLiberator
+import de.binarynoise.liberator.SSID
 import de.binarynoise.util.okhttp.checkSuccess
+import de.binarynoise.util.okhttp.parseJsonObject
 import de.binarynoise.util.okhttp.postForm
 import de.binarynoise.util.okhttp.requestUrl
 import okhttp3.Cookie
@@ -9,17 +11,35 @@ import okhttp3.OkHttpClient
 import okhttp3.Response
 
 @Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
+@SSID("Commerzbank-Wifi")
 object Commerzbank : PortalLiberator {
     override fun canSolve(response: Response): Boolean {
-        return "wifiaccess.co" == response.requestUrl.host && response.requestUrl.pathSegments.lastOrNull() == "portal"
+        return response.requestUrl.host == "wifiaccess.co" // 
+            && response.requestUrl.pathSegments.getOrNull(1) == "portal" // 
+            && !response.isRedirect
     }
     
     override fun solve(client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
         client.postForm(response.requestUrl, "/portal_api.php", mapOf("action" to "init")).checkSuccess()
+        val json1 = client.postForm(
+            response.requestUrl,
+            "/portal_api.php",
+            mapOf(
+                "action" to "subscribe",
+                "type" to "one",
+                "policy_accept" to "true",
+            ),
+        ).parseJsonObject()
+        val subscribeObject = json1.getJSONObject("info").getJSONObject("subscribe")
         client.postForm(
             response.requestUrl,
             "/portal_api.php",
-            mapOf("action" to "subscribe", "type" to "one", "policy_accept" to "true"),
+            mapOf(
+                "action" to "authenticate",
+                "login" to subscribeObject.getString("login"),
+                "password" to subscribeObject.getString("password"),
+                "policy_accept" to "true",
+            ),
         ).checkSuccess()
     }
 }
