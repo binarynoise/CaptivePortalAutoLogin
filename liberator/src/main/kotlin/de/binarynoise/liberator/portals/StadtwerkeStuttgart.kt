7599a24e54cfd4327e7b2d6d96aad68cea4e559c
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-02-21 15:39:47 +0100

    fix Solarbank

--- liberator/src/main/kotlin/de/binarynoise/liberator/portals/StadtwerkeStuttgart.kt
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/StadtwerkeStuttgart.kt
@@ -4,34 +4,29 @@ import de.binarynoise.liberator.Experimental
 import de.binarynoise.liberator.PortalLiberator
 import de.binarynoise.liberator.SSID
 import de.binarynoise.util.okhttp.checkSuccess
-import de.binarynoise.util.okhttp.firstPathSegment
-import de.binarynoise.util.okhttp.hasQueryParameter
+import de.binarynoise.util.okhttp.get
 import de.binarynoise.util.okhttp.isIp
-import de.binarynoise.util.okhttp.postForm
+import de.binarynoise.util.okhttp.lastPathSegment
+import de.binarynoise.util.okhttp.parseHtml
 import de.binarynoise.util.okhttp.requestUrl
+import de.binarynoise.util.okhttp.toParameterMap
 import okhttp3.Cookie
 import okhttp3.OkHttpClient
 import okhttp3.Response
+import org.jsoup.nodes.FormElement
 
 @Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
 @Experimental
-@SSID("SOLARBANK-WLAN", mustMatch = true)
+@SSID("Solarbank-WLAN", mustMatch = true)
 object StadtwerkeStuttgart : PortalLiberator {
     override fun canSolve(response: Response): Boolean {
-        return response.requestUrl.isIp // 
-            && response.requestUrl.firstPathSegment == "login" // 
-            && response.requestUrl.hasQueryParameter("dst")
+        return response.requestUrl.isIp && response.requestUrl.lastPathSegment == "macauth"
     }
     
     override fun solve(client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
-        val dst = response.requestUrl.queryParameter("dst") ?: error("no dst")
-        client.postForm(
-            response.requestUrl, null, mapOf(
-                "dst" to dst,
-                "popup" to "false",
-                "username" to "anonymous",
-                "password" to "anonymous",
-            )
-        ).checkSuccess()
+        val html = response.parseHtml()
+        val form = html.getElementsByTag("form").single() as FormElement
+        val url = form.attr("action").takeIf { it.isNotEmpty() } ?: error("no form action url")
+        client.get(response.requestUrl, url, form.toParameterMap()).checkSuccess()
     }
 }
