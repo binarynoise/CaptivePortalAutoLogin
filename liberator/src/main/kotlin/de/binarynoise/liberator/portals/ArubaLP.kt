9bc33ea3c8d4395883c916d57db9a36291734d6e
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-02-24 16:25:12 +0100

    group ArubaLP networks and implement Segmüller

--- liberator/src/main/kotlin/de/binarynoise/liberator/portals/ArubaLP.kt
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/ArubaLP.kt
@@ -11,7 +11,12 @@ import okhttp3.Cookie
 import okhttp3.OkHttpClient
 import okhttp3.Response
 
-@Experimental
+/*
+ArubaLP networks can be identified by the initial response being 200 OK,
+then with a meta refresh it redirects to a url containing cmd=redirect&arubalp=12345
+https://support.hpe.com/hpesc/public/docDisplay?docId=sf000095199en_us&docLocale=en_US
+*/
+
 @SSID("PRIMARK_PUBLIC")
 object Primark : PortalLiberator {
     override fun canSolve(response: Response): Boolean {
@@ -25,6 +30,28 @@ object Primark : PortalLiberator {
             mapOf(
                 "cmd" to "authenticate",
                 "email" to "Customer@wifi.primark.net",
+            ),
+        )
+    }
+}
+
+@Experimental
+@SSID("Segmueller-Hotspot")
+object Segmueller : PortalLiberator {
+    override fun canSolve(response: Response): Boolean {
+        return response.requestUrl.host == "hotspot.segmueller.de"
+    }
+    
+    override fun solve(client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
+        client.postForm(
+            null,
+            "https://captiveportal-login.segmueller.de/cgi-bin/login",
+            mapOf(
+                "cmd" to "authenticate",
+                "url" to "http://www.segmueller.de",
+                "user" to "SEG_Anonymous",
+                "Login" to "Log In",
+                "password" to "069424",
             ),
         )
     }
