c720ee005f882ffcfdeef34fa71437743ea24c48
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-02-25 14:24:16 +0100

    fix MediaMarktSaturn and rename to CiscoMobility

--- liberator/src/main/kotlin/de/binarynoise/liberator/portals/CiscoWirelessMobility.kt
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/CiscoWirelessMobility.kt
@@ -1,20 +1,29 @@
+@file:Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
 package de.binarynoise.liberator.portals
 
 import de.binarynoise.liberator.PortalLiberator
 import de.binarynoise.liberator.SSID
 import de.binarynoise.util.okhttp.checkSuccess
 import de.binarynoise.util.okhttp.decodedPath
+import de.binarynoise.util.okhttp.hasQueryParameter
 import de.binarynoise.util.okhttp.postForm
 import de.binarynoise.util.okhttp.requestUrl
 import okhttp3.Cookie
 import okhttp3.OkHttpClient
 import okhttp3.Response
 
-@SSID("media-kunden")
-@Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
-object MediaMarktSaturn : PortalLiberator {
+@SSID(
+    "media-kunden",
+    "saturn-kunden",
+)
+object CiscoWirelessMobility : PortalLiberator {
+    // https://www.cisco.com/c/en/us/support/docs/wireless-mobility/wireless-lan-wlan/118826-config-https-webauth-00.html
     override fun canSolve(response: Response): Boolean {
-        return "192.0.2.1" == response.requestUrl.host && response.requestUrl.decodedPath == "fs/customwebauth/login.html"
+        return with(response.requestUrl) {
+            decodedPath == "/fs/customwebauth/login.html" //
+                && hasQueryParameter("switch_url") // 
+                && hasQueryParameter("redirect")
+        }
     }
     
     override fun solve(client: OkHttpClient, response: Response, cookies: Set<Cookie>) {
