462c281541da5891b4fb9b6b963a7c7c700c241b
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-04-05 12:56:46 +0200

    implement DerTour
    again one of those "`submitOnlyForm`" portals

---
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/DerTour.kt
@@ -0,0 +1,27 @@
+@file:Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
+
+package de.binarynoise.liberator.portals
+
+import de.binarynoise.liberator.Experimental
+import de.binarynoise.liberator.LiberatorExtras
+import de.binarynoise.liberator.PortalLiberator
+import de.binarynoise.liberator.SSID
+import de.binarynoise.util.okhttp.checkSuccess
+import de.binarynoise.util.okhttp.firstPathSegment
+import de.binarynoise.util.okhttp.isIp
+import de.binarynoise.util.okhttp.requestUrl
+import de.binarynoise.util.okhttp.submitOnlyForm
+import okhttp3.OkHttpClient
+import okhttp3.Response
+
+@Experimental
+@SSID("DERTOUR", mustMatch = true)
+object DerTour : PortalLiberator {
+    override fun canSolve(response: Response): Boolean {
+        return response.requestUrl.isIp && response.requestUrl.firstPathSegment == "authen"
+    }
+    
+    override fun solve(client: OkHttpClient, response: Response, extras: LiberatorExtras) {
+        response.submitOnlyForm(client).checkSuccess()
+    }
+}
