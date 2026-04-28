17a374e7e5268651f46b789ef586fd155ceb3335
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-04-28 16:12:57 +0200

    refactor into LPHotspot
    some commonization found,
    sadly unable to derive correct name

--- liberator/src/main/kotlin/de/binarynoise/liberator/portals/LPHotspot.kt
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/LPHotspot.kt
@@ -5,15 +5,31 @@ package de.binarynoise.liberator.portals
 import de.binarynoise.liberator.LiberatorExtras
 import de.binarynoise.liberator.PortalLiberator
 import de.binarynoise.liberator.SSID
+import de.binarynoise.util.okhttp.parseHtml
 import de.binarynoise.util.okhttp.postForm
 import de.binarynoise.util.okhttp.requestUrl
 import okhttp3.OkHttpClient
 import okhttp3.Response
 
-@SSID("MesseSpot")
-object MaxxArena : PortalLiberator {
+/**
+ * We currently don't know what the portal service provider for this portal is named.
+ * For now this [PortalLiberator] is named after the many "lp" classes present in their HTML.
+ */
+@SSID(
+    "Landratsamt Besucher",
+    "MesseSpot",
+    "Vector Hotspot",
+)
+object LPHotspot : PortalLiberator {
     override fun canSolve(response: Response): Boolean {
-        return response.requestUrl.host == "hotspot.maxxarena.de"
+        if (!response.isSuccessful) return false
+        val html = response.parseHtml()
+        return listOf(
+            "div.lp-content",
+            "div.lp-content-box",
+            "div.lp-loading",
+            "form.lp-form",
+        ).all { html.selectFirst(it) != null }
     }
     
     override fun solve(client: OkHttpClient, response: Response, extras: LiberatorExtras) {
