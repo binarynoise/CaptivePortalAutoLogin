25d2288655cbd4719ddcf6aa7431f47c8ee66ed9
binarynoise <binarynoise@mail.de>
2026-06-10 20:45:30 +0200

    handle internet usage blocked by FRITZ!Box

--- liberator/src/main/kotlin/de/binarynoise/liberator/portals/FritzBox.kt
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/FritzBox.kt
@@ -2,8 +2,9 @@ package de.binarynoise.liberator.portals
 
 import de.binarynoise.liberator.LiberatorExtras
 import de.binarynoise.liberator.PortalLiberator
+import de.binarynoise.liberator.UnsupportedPortalException
 import de.binarynoise.util.okhttp.checkSuccess
-import de.binarynoise.util.okhttp.firstPathSegment
+import de.binarynoise.util.okhttp.decodedPath
 import de.binarynoise.util.okhttp.get
 import de.binarynoise.util.okhttp.requestUrl
 import okhttp3.OkHttpClient
@@ -12,10 +13,21 @@ import okhttp3.Response
 @Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
 object FritzBox : PortalLiberator {
     override fun canSolve(response: Response): Boolean {
-        return "untrusted_guest.lua" == response.requestUrl.firstPathSegment
+        return "untrusted_guest.lua" == response.requestUrl.decodedPath
     }
     
     override fun solve(client: OkHttpClient, response: Response, extras: LiberatorExtras) {
         client.get(response.requestUrl, "/trustme.lua?accept=").checkSuccess()
+    }
+}
+
+@Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
+object FritzBoxBlocked : PortalLiberator {
+    override fun canSolve(response: Response): Boolean {
+        return "fritz.box" == response.requestUrl.host && "blocked" == response.requestUrl.decodedPath
+    }
+    
+    override fun solve(client: OkHttpClient, response: Response, extras: LiberatorExtras) {
+        throw UnsupportedPortalException("FRITZ!Box blocks internet access")
     }
 }
