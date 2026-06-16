137d20dcbcb96e02c10bccef3dba9c614ac4b250
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-06-16 16:46:28 +0200

    implement ColesFreeWifi and refactor CiscoWirelessMobility

--- liberator/src/main/kotlin/de/binarynoise/liberator/portals/CiscoWirelessMobility.kt
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/CiscoWirelessMobility.kt
@@ -1,6 +1,8 @@
 @file:Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
+
 package de.binarynoise.liberator.portals
 
+import de.binarynoise.liberator.Experimental
 import de.binarynoise.liberator.LiberatorExtras
 import de.binarynoise.liberator.PortalLiberator
 import de.binarynoise.liberator.SSID
@@ -9,15 +11,18 @@ import de.binarynoise.util.okhttp.decodedPath
 import de.binarynoise.util.okhttp.hasQueryParameter
 import de.binarynoise.util.okhttp.postForm
 import de.binarynoise.util.okhttp.requestUrl
+import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Response
+
+// https://www.cisco.com/c/en/us/support/docs/wireless-mobility/wireless-lan-wlan/118826-config-https-webauth-00.html
+// https://github.com/stuartst/cisco-wlc-captive-portal/blob/master/README.md
 
 @SSID(
     "media-kunden",
     "saturn-kunden",
 )
 object CiscoWirelessMobility : PortalLiberator {
-    // https://www.cisco.com/c/en/us/support/docs/wireless-mobility/wireless-lan-wlan/118826-config-https-webauth-00.html
     override fun canSolve(response: Response): Boolean {
         return with(response.requestUrl) {
             decodedPath == "/fs/customwebauth/login.html" //
@@ -29,14 +34,49 @@ object CiscoWirelessMobility : PortalLiberator {
     override fun solve(client: OkHttpClient, response: Response, extras: LiberatorExtras) {
         val switch_url = response.requestUrl.queryParameter("switch_url") ?: error("no login_url")
         val redirect_url = response.requestUrl.queryParameter("redirect") ?: error("no redirect_url")
+        solve(client, response.requestUrl, switch_url, redirect_url)
+    }
+    
+    fun solve(
+        client: OkHttpClient,
+        baseUrl: HttpUrl,
+        switch_url: String,
+        redirect_url: String,
+        additionalParameters: Map<String, String> = mapOf(),
+    ) {
         client.postForm(
-            response.requestUrl, switch_url,
+            baseUrl,
+            switch_url,
             mapOf(
-                "del[]" to "on",
                 "redirect_url" to redirect_url,
+                "buttonClicked" to "4", // mandatory
                 "err_flag" to "0",
-                "buttonClicked" to "4",
-            ),
+            ) + additionalParameters,
         ).checkSuccess()
+    }
+}
+
+@Experimental
+@SSID("MBMuseum_FreeWifi")
+object MercedesBenzMuseum : PortalLiberator {
+    override fun canSolve(response: Response): Boolean {
+        return response.requestUrl.host == "wifi.media.mercedes-benz.museum"
+    }
+    
+    override fun solve(client: OkHttpClient, response: Response, extras: LiberatorExtras) {
+        CiscoWirelessMobility.solve(client, response.requestUrl, "", "success.html")
+    }
+}
+
+@Experimental
+@SSID("ColesFreeWiFi")
+object Coles : PortalLiberator {
+    override fun canSolve(response: Response): Boolean {
+        return response.requestUrl.host == "app.coles.com.au" //
+            && response.requestUrl.decodedPath == "/colesfreewifi.htm"
+    }
+    
+    override fun solve(client: OkHttpClient, response: Response, extras: LiberatorExtras) {
+        CiscoWirelessMobility.solve(client, response, extras)
     }
 }
