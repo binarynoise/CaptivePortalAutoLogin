2b7c60d19369c85fa596690ab7a64159c8c011de
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-06-23 15:32:50 +0200

    implement CloudAuth

---
+++ liberator/src/main/kotlin/de/binarynoise/liberator/portals/CloudAuth.kt
@@ -0,0 +1,28 @@
+@file:Suppress("SpellCheckingInspection", "GrazieInspection", "LocalVariableName", "RedundantSuppression")
+
+package de.binarynoise.liberator.portals
+
+import de.binarynoise.liberator.Experimental
+import de.binarynoise.liberator.LiberatorExtras
+import de.binarynoise.liberator.PortalLiberator
+import de.binarynoise.liberator.SSID
+import de.binarynoise.util.okhttp.decodedPath
+import de.binarynoise.util.okhttp.requestUrl
+import de.binarynoise.util.okhttp.submitOnlyForm
+import okhttp3.OkHttpClient
+import okhttp3.Response
+
+@Experimental
+@SSID(
+    "pap_guest",
+)
+object CloudAuth : PortalLiberator {
+    override fun canSolve(response: Response): Boolean {
+        return response.requestUrl.host.endsWith(".aio.cloudauth.net") //
+            && response.requestUrl.decodedPath == "/swarm.cgi"
+    }
+    
+    override fun solve(client: OkHttpClient, response: Response, extras: LiberatorExtras) {
+        response.submitOnlyForm(client)
+    }
+}
