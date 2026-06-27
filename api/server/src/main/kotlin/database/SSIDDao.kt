fb7da200943c14bde2a4f15ee62ec1efe59ee1ac
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-06-27 16:39:29 +0200

    implement ssid endpoint limit by majorversion

--- api/server/src/main/kotlin/database/SSIDDao.kt
+++ api/server/src/main/kotlin/database/SSIDDao.kt
@@ -9,10 +9,15 @@ interface SSIDDao {
         """
         SELECT ssid 
         FROM successes
+        WHERE version NOT LIKE '%+%'
+        AND CAST(SUBSTR(version, 1, INSTR(version, '-') - 1) AS INTEGER) <= :majorVersion
         GROUP BY ssid
         ORDER BY COUNT(*) DESC
         LIMIT :limit
         """
     )
-    suspend fun getSSIDs(limit: Int = 1024): List<String>
+    suspend fun getSSIDs(
+        limit: Int = 1024,
+        majorVersion: Int = Int.MAX_VALUE,
+    ): List<String>
 }
