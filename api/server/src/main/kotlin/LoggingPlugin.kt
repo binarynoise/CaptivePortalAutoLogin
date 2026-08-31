d4e89a3ebdba9369e7c3b10acb88e2c87e10bbba
binarynoise <binarynoise@mail.de>
2026-08-31 19:12:55 +0200

    fix the statuscode

--- api/server/src/main/kotlin/LoggingPlugin.kt
+++ api/server/src/main/kotlin/LoggingPlugin.kt
@@ -6,8 +6,12 @@ import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.ApplicationPlugin
 import io.ktor.server.application.createApplicationPlugin
 import io.ktor.server.application.hooks.CallFailed
+import io.ktor.server.application.hooks.ResponseSent
 import io.ktor.server.plugins.origin
 import io.ktor.server.request.httpMethod
+import io.ktor.util.AttributeKey
+
+private val ResponseBodyKey = AttributeKey<Any>("LoggingPlugin.responseBody")
 
 val LoggingPlugin: ApplicationPlugin<Unit> = createApplicationPlugin(name = "LoggingPlugin") {
     onCallReceive { call, body ->
@@ -15,13 +19,17 @@ val LoggingPlugin: ApplicationPlugin<Unit> = createApplicationPlugin(name = "Log
         Logger.log("receiving call to ${call.request.httpMethod.value} ${call.request.origin.uri} with body $body")
     }
     onCallRespond { call, body ->
+        call.attributes.put(ResponseBodyKey, body)
+    }
+    on(ResponseSent) { call ->
+        val body = call.attributes.getOrNull(ResponseBodyKey)
         Logger.log(buildString {
-            append("responding to call ")
+            append("responded to call ")
             append(call.request.httpMethod.value)
             append(" ")
             append(call.request.origin.uri)
             append(" with status code ")
-            append(call.response.status()?.value ?: 200)
+            append(call.response.status()?.value)
             append(" and body '")
             append(body.toString().substringBefore("\n").take(100))
             append("'")
